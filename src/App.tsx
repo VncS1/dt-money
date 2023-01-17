09195cd56ea9ca3bd0fabedb43cd124d6b9f8b3f
Vinicius M <viniciusmachioni79@gmail.com>
2023-01-17 14:44:11 -0300

    Componentes criados e estilizados

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components";
+import { Transactions } from "./pages/Transactions";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
@@ -6,7 +7,8 @@ import { defaultTheme } from "./styles/themes/default";
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>a</h1>
+      <Transactions />
+
       <GlobalStyle />
     </ThemeProvider>
   )
